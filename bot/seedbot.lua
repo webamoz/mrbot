055a5cbf113cb69add4bc5b4b2b1225a4fc29146
webamoz <mahnaz.9131@gmail.com>
2016-01-12 04:31:58 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {152205054},--Sudo users
+    sudo_users = {178275371},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
